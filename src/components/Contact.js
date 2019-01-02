72feff3e82d20f1b15591c19200d1c220a9f9f93
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-01-02 13:16:13 -0600

    added HomePage Component and Path in AppRouter.js

--- src/components/Contact.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Contact = (props) => {
-
-};
-
-export default Contact;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import Contact from '../components/Contact';
-import Portfolio from '..components/Portfolio';
-import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
+import ContactPage from '../components/ContactPage';
+import HomePage from '..components/HomePage';
+import NotFoundPage from '../components/NotFoundPage';
+
 
 const AppRouter = () => (
 <BrowserRouter>
    <div>
       <Header />
       <Switch>
-         <Route  />
+         <Route path="/" component={HomePage} />
          <Route  />
          <Route  />
          <Route component={NotFoundPage} />  
